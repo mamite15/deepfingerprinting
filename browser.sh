cee4a66a3e262871ff40c25747b165bde011798c
mamite15 <mamite15>
2023-11-09 11:13:26 +0900

    second

--- browser.sh
+++ browser.sh
@@ -12,15 +12,15 @@ do
         #python browser_usingshell.py $line &
         #sudo tcpdump -s 0 -U -w ./scap/$cnt.pcap port 443 
 
-        while true
-        do
-            if [ $? -eq 0 ]; then
-                let cnt=$cnt+1
-                #sudo kill $!
-                wait $!
-                break
-            #echo $line
-            fi
-        done
+        #while true
+        #do
+        if [ $? -eq 0 ]; then
+            let cnt=$cnt+1
+            #sudo kill $!
+            wait $!
+            break
+        #echo $line
+        fi
+        #done
     done < ./test_url_list.txt
 done
